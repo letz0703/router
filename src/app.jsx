2ed8676b05b7328810fc43908cb7bb3920c24f5b
ManI <letz0703@gmail.com>
2022-06-27 00:03:18 +0900

    220627SUN.design : Main Scrollout

--- src/app.jsx
+++ src/app.jsx
@@ -3,6 +3,7 @@ import Home from './components/home';
 import Profile from './components/profile';
 import React, {Component} from 'react';
 import ScrollOut from 'scroll-out';
+// scrollout lecture : https://www.youtube.com/watch?v=m-MpXGFKomE
 import styles from './app.module.css';
 
 export default class App extends Component {
@@ -19,58 +20,49 @@ export default class App extends Component {
   render() {
     return (
       <>
-        <section scroll-down className={styles.title}>
-          <h1>hi</h1>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit quam
-            consequatur, tempora corporis saepe esse, natus tenetur, voluptas id
-            ratione quis perspiciatis exercitationem iste? Officia ad,
-            reiciendis facere totam mollitia voluptas eveniet ab. Non saepe
-            ipsum ab soluta! Ullam vel iure dignissimos quis, maxime dolore?
-            Tempora officiis voluptas sed dicta quibusdam explicabo? Neque
-            accusamus saepe obcaecati sit similique dolor commodi corporis
-            ipsum! Eius velit maiores dolor rem alias debitis aliquam, dolorum
-            adipisci perferendis incidunt soluta, placeat id nisi perspiciatis
-            quam sed hic libero animi est obcaecati ipsum asperiores? Rerum
-            ducimus iure iste quidem at! Eaque fugit perspiciatis molestias
-            maxime at odio itaque praesentium ad nostrum! Repellat asperiores
-            hic excepturi labore nisi, veritatis accusamus, nostrum maiores
-            nobis est adipisci quisquam iste impedit? Recusandae porro quam
-            aliquid! Nulla, repellendus iusto sed optio quidem similique nostrum
-            sapiente voluptatum fuga dolore modi, omnis, qui illo deleniti dolor
-            distinctio blanditiis amet enim odio harum animi. Aut debitis vel
-            nulla sapiente accusantium, ipsum nam est doloremque repellat ipsa
-            laborum? Aspernatur blanditiis voluptate, recusandae facilis minus
-            facere voluptatum culpa tempore suscipit, id sint amet eaque ea non
-            eius. Blanditiis est magni, fugiat dolorum laborum maxime amet,
-            error assumenda aspernatur atque alias ea, nobis nostrum quisquam
-            qui voluptatibus.
-          </p>
+        <section data-scroll>
+          <div className={styles.page}>
+            <h1>shop JAPAN</h1>
+            <p
+              className='${styles.text} ${styles.text--random} f1 pb3 gray'
+              data-scroll='out'
+              data-splitting=''
+            >
+              Order For You{' '}
+            </p>
+            <div className='f2'>
+              <ul>
+                <li>일본 주문대행.</li>
+                <li>물건은 다음날 받으세요!</li>
+              </ul>
+            </div>
+            <p className='black pt4 description'>
+              일본 쇼핑몰에 주문을 하면 최소 일주일에서 보름이 지나야 상품을
+              받습니다. 배송비도 엄청나지요.
+              <br /> 한두개만 주문하신다면 저희가 먼저 물건을 보내 드리겠습니다.
+              <br /> 주문하고 기다리는 것은 저희가 하겠습니다.
+            </p>
+            <button href='#' className='bg-red white pv3 mt2 ph3 f3'>
+              주문대행 조건 상세보기
+            </button>
+          </div>
         </section>
-        <section scroll-down>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio illum
-          iusto rerum aut iste possimus repellat, fugiat quasi veniam non
-          veritatis adipisci accusamus nostrum voluptas officiis neque, quas
-          ipsa esse, reiciendis modi! Sapiente, quo quis, sunt maiores quisquam
-          odio magnam, nemo rerum hic inventore facilis enim quas nisi. Optio,
-          fuga tempore! Architecto quos est sapiente porro impedit nulla
-          repudiandae quae, placeat vero magni earum. Perspiciatis animi
-          praesentium minus ullam nihil aspernatur rerum deserunt dicta,
-          veritatis vitae voluptatibus corrupti iste debitis fuga blanditiis
-          quaerat aperiam libero rem maxime labore! Perspiciatis consequuntur,
-          sequi non corrupti ipsum nesciunt ea perferendis hic optio cum in
-          dolore omnis beatae aliquam vero ad. Voluptatibus, in eius consectetur
-          iure odio pariatur modi asperiores doloremque a voluptas minima
-          aliquam ipsum nostrum consequatur numquam doloribus perferendis
-          similique. Earum, ex? Amet tenetur aliquid quae et, expedita nostrum.
-          Mollitia maiores sapiente vero facere beatae omnis id asperiores. Ex
-          sapiente corrupti illo enim in laboriosam sit debitis. Asperiores,
-          temporibus laborum? Sed magnam, numquam debitis nobis corrupti vero
-          facilis fugiat alias odio eveniet repudiandae at facere deleniti quasi
-          necessitatibus unde quia nostrum ipsum reprehenderit vitae ea aut.
-          Consectetur facilis recusandae nam omnis amet autem nobis ullam
-          doloribus, facere aliquam velit architecto placeat dolores.
+        <section data-scroll>
+          <div className={styles.page}>
+            <h1>buy TOGETHER</h1>
+            <div
+              className='${styles.text} ${styles.text--random} f1 pb3 ${styles.gonggoo}'
+              data-scroll='out'
+              data-splitting=''
+            >
+              order 0909{' '}
+            </div>
+            <div className='f3'>
+              관심있는 상품을 올리고 함께 공동구매(준비중)
+            </div>
+          </div>
         </section>
+
         <BrowserRouter>
           <Switch>
             <Route path='/home'>
