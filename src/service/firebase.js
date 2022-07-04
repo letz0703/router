9f65f0263181e0f5695e96dca8e019f3b8aff7e6
ManI <letz0703@gmail.com>
2022-07-05 00:47:07 +0900

    Set firebase 220705

---
+++ src/service/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_API_KEY,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGE_SENDER_ID
+};
+
+//Initialize Firebase
+const app = initializeApp(firebaseConfig);
